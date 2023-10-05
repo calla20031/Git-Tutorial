00c804a9ea7e203bc953add0462a4bad53cde6c8
calla20031 <127398251+calla20031@users.noreply.github.com>
2023-10-05 19:02:32 +0900

    Add files via upload

---
+++ hw3.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main(void)
+{
+	int i,j,k;
+    for (int i = 0; i < 5; i++) 
+    {
+        for (int j = 0; j < 5 - i - 1; j++) 
+        {
+            printf(" ");
+        }
+        for (int k = 0; k < 2 * i + 1; k++) 
+        {
+            printf("*");
+        }
+        printf("\n");
+	}
+	return 0;
+
+}
